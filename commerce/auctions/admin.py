6a5ba7a572a0d55780b7d98d92cd0f8887f19222
Vaidehi Desai <vaidesai171202@gmail.com>
2022-06-22 14:22:01 +0530

    Delete commerce directory
    Removed the commerce project

--- commerce/auctions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Comment, Bid, Listing, Category, User
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Comment)
-admin.site.register(Bid)
-admin.site.register(Listing)
-admin.site.register(Category)
